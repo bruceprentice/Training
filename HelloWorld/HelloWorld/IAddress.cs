af6ff716ce04a5e0f42d0a4adbe66f5d409e95e9
Bruce <bruce.prentice@outlook.com>
2016-08-28 14:26:33 -0500

    Added feature for geolocation

--- HelloWorld/HelloWorld/IAddress.cs
+++ HelloWorld/HelloWorld/IAddress.cs
@@ -1,6 +1,16 @@
-﻿namespace HelloWorld
+﻿using System.Device.Location;
+
+namespace HelloWorld
 {
     public interface IAddress
     {
+        string AddressLine1 { get; set; }
+        string AddressLine2 { get; set; }
+        string City { get; set; }
+        string State { get; set;  }
+        string Province { get; set; }
+        string PostalCode { get; set; }
+        GeoCoordinate geoCode { get; set; }
+
     }
 }
